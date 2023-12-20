fe6974054b468a2ce8040e72b05fca1a7ba2e6d5
Alexander Seiler <alexander55566@gmx.at>
2023-12-20 21:50:36 +0100

    Day 2 changes

---
+++ src/Day3-1.kt
@@ -0,0 +1,21 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+
+    val inputString = File("inputs/day3.txt").readText()
+
+
+    var inputStringSplit = inputString.split("\r\n")
+    var inputStringSplit4 = inputStringSplit[4]
+    println(inputStringSplit4)
+
+
+
+
+
+
+
+
+
+}
+
